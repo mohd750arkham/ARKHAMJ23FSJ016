de19931a74e3c9c1a6cc49aaedd5e56eefcb4747
Arkham Suhail <98503450+mohd750arkham@users.noreply.github.com>
2023-07-13 22:35:37 +0530

    Add files via upload

---
+++ CheckPrime.java
@@ -0,0 +1,31 @@
+package openlabbook1;
+
+import java.util.Scanner;
+
+public class CheckPrime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int temp;
+		boolean isPrime = true;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter any number:");
+		
+		int num = scan.nextInt();
+		scan.close();
+		for(int i = 2; i<=num/2;i++)
+		{
+			temp = num%i;
+			if(temp == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+		if(isPrime)
+			System.out.println(num + " is a Prime number");
+		else 
+			System.out.println(num + " is not a Prime number");
+	}
+
+}
--- ReverseANumber.java
+++ ReverseANumber.java
@@ -1,10 +1,24 @@
 package openlabbook1;
 
+import java.util.Scanner;
+
 public class ReverseANumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int num = 0;
+		int reversenum = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Input the No and press Enter: ");
+		 num = sc.nextInt();
+		 
+		 while(num != 0) {
+			 reversenum = reversenum * 10;
+			 reversenum = reversenum + num%10;
+			 num = num/10; 
+					 
+		 }
+		 System.out.println("Reverse of input number is : " + reversenum);
 	}
 
 }
