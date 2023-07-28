a601984c4a6f600ad18ac08242e2c0d39209075c
Arkham Suhail <98503450+mohd750arkham@users.noreply.github.com>
2023-07-28 21:50:18 +0530

    Add files via upload

---
+++ Program12.java
@@ -0,0 +1,11 @@
+package closedLabBook1;
+
+public class Program12 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<args.length;i++) {
+			System.out.println(args[i]);
+		}
+	}
+}
